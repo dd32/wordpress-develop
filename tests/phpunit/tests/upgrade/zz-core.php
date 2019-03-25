85255e81f42f642c67af1105030572538cac25d8
Dion Hulse <contact@dd32.id.au>
2019-03-25 18:16:40 +1100

    Core Update tests, round 2

--- tests/phpunit/tests/upgrade/zz-core.php
+++ tests/phpunit/tests/upgrade/zz-core.php
@@ -19,17 +19,54 @@ class Core_Upgrader_Tests extends WP_Upgrader_UnitTestCase {
 			$messages[] = $message;
 			return $message;
 		};
-		add_filter( 'feedback', $message_recorder, 1, 1 );
+		add_filter( 'update_feedback', $message_recorder, 1, 1 );
 
 		ob_start();
+		// Ew. This can only be done once..
+		include ABSPATH . 'wp-admin/upgrade-core.php';
 		do_core_upgrade( true );
 		$output = ob_end_clean();
 
-		remove_filter( 'feedback', $message_recorder, 1 );
+		remove_filter( 'update_feedback', $message_recorder, 1 );
 
 		var_dump( $output, $messages );
 
 		$this->assertContains( 'WordPress updated successfully', $output );
 	}
 
+	/**
+	 * Integration test - Test a reinstall of WordPress.
+	 * @group upgrade-tests
+	 */
+	function test_core_reinstall_alt() {
+		if ( ! getenv( 'WP_TRAVISCI' ) ) {
+			$this->markTestSkipped( "We don't appear to be running in a travis environment." );
+		}
+
+		// $messages contains the during-upgrade texts, $output below will contain the success/fail messages.
+		$messages = [];
+		$message_recorder = function( $message ) use ( $messages ) {
+			$messages[] = $message;
+			return $message;
+		};
+		add_filter( 'update_feedback', $message_recorder, 1, 1 );
+
+		$update  = find_core_update( false, 'en_US' );
+		WP_Filesystem( [], ABSPATH, true );
+		$update->response = 'reinstall';
+		$upgrader = new Core_Upgrader();
+		$result   = $upgrader->upgrade(
+			$update,
+			array(
+				'allow_relaxed_file_ownership' => true,
+			)
+		);
+
+		remove_filter( 'update_feedback', $message_recorder, 1 );
+
+		var_dump( $result, $messages );
+
+		$this->assertTrue( $result );
+
+	}
 }
