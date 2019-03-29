66eb8a53669d30a2342cd9e61ee24eb0563f9c55
Dion Hulse <contact@dd32.id.au>
2019-03-29 15:50:19 +1100

    Add Plugin Upgrade checks

--- tests/phpunit/tests/upgrade/plugins.php
+++ tests/phpunit/tests/upgrade/plugins.php
@@ -7,12 +7,48 @@ class Plugin_Upgrader_Tests extends WP_Upgrader_UnitTestCase {
 	 */
 	function test_install_plugin_hello_dolly() {
 		$messages = $this->install_plugin_and_return_messages( 'hello-dolly' );
-
 		$this->assertContains( 'Plugin installed successfully.', $messages );
 
 		// Now install it again, it should fail.
 		$messages = $this->install_plugin_and_return_messages( 'hello-dolly' );
-
 		$this->assertNotContains( 'Plugin installed successfully.', $messages );
+	}
+
+	/**
+	 * Integration test - Install a plugin, check updates pass.
+	 * @group upgrade-tests
+	 */
+	function test_update_plugin_hello_dolly() {
+		$hello_dolly = WP_PLUGIN_DIR . '/hello-dolly/hello.php';
+		// If the plugins already installed from another test, save some time.
+		if ( ! file_exists( $hello_dolly ) ) {
+			$messages = $this->install_plugin_and_return_messages( 'hello-dolly' );
+			$this->assertContains( 'Plugin installed successfully.', $messages );
+		}
+
+		// Lower the version string of the plugin..
+		$contents = file_get_contents( $hello_dolly );
+		$contents = preg_replace_callback( '!^([\s*]*Version:\s*)([\d.]+)$!im', function( $m ) {
+			return $m[1] . ( floatval( $m[2] ) - 0.1 );
+		}, $contents );
+		file_put_contents( $hello_dolly, $contents );
+
+		// Get the update..
+		wp_update_plugins();
+
+		$skin     = new WP_Tests_Upgrader_Skin();
+		$upgrader = new Plugin_Upgrader( $skin );
+		$result = $upgrader->upgrade(
+			plugin_basename( $hello_dolly )
+		);
+
+		$messages = $skin->get_upgrade_messages();
+
+		$this->assertTrue( $result );
+		$this->assertContains( 'Plugin updated successfully.', $messages );
+	}
+
+	function tearDown() {
+		delete_plugins( [ 'hello-dolly/hello.php' ] );
 	}
 }
