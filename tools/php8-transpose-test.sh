c0a7e1768b0fcd5f8c9567546e22711fc43298ba
Dion Hulse <contact@dd32.id.au>
2020-08-18 13:17:13 +1000

    Add some void functions.

--- tools/php8-transpose-test.sh
+++ tools/php8-transpose-test.sh
@@ -10,4 +10,11 @@ RUN curl -sL https://phar.phpunit.de/phpunit-9.phar > /usr/local/bin/phpunit && 
 # Use a PHP8 compatible PHPunit.
 sed -i 's!phpunit:$!phpunit:\n    build:\n      context: .\n      dockerfile: php8.dockerFile!i' docker-compose.yml
 
-cat docker-compose.yml
+# We need to use some PHP 7.2+ syntax for PHPUnit 9
+
+# these functions must be return void as of PHPUnit8
+for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
+do
+  echo Converting ${void_function}
+  grep "function\s*${void_function}()\s*{" tests/phpunit/ -rli | xargs sed -i "s!function\s*${void_function}()\s*{!function ${void_function}(): void /* PHP8 transpose */!gi"
+done
