496061d60836fbebe891c6614ac7110e93eb1f9f
Dion Hulse <contact@dd32.id.au>
2020-08-18 12:48:10 +1000

    Move dockerfile inline for simplicity

--- tools/php8-transpose-test.sh
+++ tools/php8-transpose-test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# Make a PHP8 compatible phpunit file.
+echo "FROM wordpressdevelop/phpunit:8.0-fpm-latest
+
+# Use PHPUnit 8
+RUN -c curl -sL https://phar.phpunit.de/phpunit-8.phar > /usr/local/bin/phpunit && chmod +x /usr/local/bin/phpunit
+" > php8.dockerFile
+
 # Use a PHP8 compatible PHPunit.
-sed -i 's!phpunit:$!phpunit:\n    build:\n      context: .\n      dockerfile: tools/php8-phpunit.dockerFile!i' docker-compose.yml
+sed -i 's!phpunit:$!phpunit:\n    build:\n      context: .\n      dockerfile: php8.dockerFile!i' docker-compose.yml
 
 cat docker-compose.yml
