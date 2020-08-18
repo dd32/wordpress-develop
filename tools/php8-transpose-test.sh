fc6553b0f65a9601dff9bccb5f518b82066c9506
Dion Hulse <contact@dd32.id.au>
2020-08-18 14:59:11 +1000

    Better comments

--- tools/php8-transpose-test.sh
+++ tools/php8-transpose-test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# The docker changes shouldn't be needed, https://github.com/WordPress/wpdev-docker-images/blob/master/update.php#L185 needs updating.
+
 # Make a PHP8 PHPUnit 9 dockerfile.
 echo "FROM wordpressdevelop/phpunit:8.0-fpm
 
@@ -10,10 +12,12 @@ RUN curl -sL https://phar.phpunit.de/phpunit-9.phar > /usr/local/bin/phpunit && 
 # Use the PHP8 + PHPUnit9 dockerfile
 sed -i 's!phpunit:$!phpunit:\n    build:\n      context: .\n      dockerfile: php8.dockerFile!i' docker-compose.yml
 
-# We need to use some PHP 7.2+ syntax for PHPUnit 9
 
-# Out bootstrap limits it to PHPUnit 7.. hacky
+# Our bootstrap limits it to PHPUnit 7.. hacky but understandable from core.
 sed -i 's/8.0/10.0/' tests/phpunit/includes/bootstrap.php
+
+# Make the needed syntax alterations to the unit tests.
+# This would ideally be done not using regex, but they're so simple statements that it's not too horrible.
 
 # these functions must be return void as of PHPUnit8
 for void_function in setUpBeforeClass setUp assertPreConditions assertPostConditions tearDown tearDownAfterClass onNotSuccessfulTest
